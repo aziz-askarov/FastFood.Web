16ad230fe1c9e3b1a5453e4b96d4e484632237ce
LatifovElbekjon <elbeklatifiy@gmail.com>
2025-10-03 17:21:57 +0500

    Add Models

---
+++ FastFood.Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Models;
+public class Cart
+{
+    [Key]
+    public int Id { get; set; }
+    public int ItemId { get; set; }
+
+    [Required,MinLength(1)]
+    public int Count { get; set; }
+    public Item Item { get; set; }
+    public int ApplicationUserId { get; set; }
+    public ApplicationUser ApplicationUser { get; set; }
+}
---
+++ FastFood.Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Models;
+public class Item
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    public double Price { get; set; }
+
+    public int Categoryid { get; set; }
+    public Category Category { get; set; }
+    public int SubCategoryid { get; set; }
+    public Category SubCategory { get; set; }
+
+}
