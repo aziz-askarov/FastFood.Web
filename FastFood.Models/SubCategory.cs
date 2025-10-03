dad2d57e70298f7d07dee321d5756a6374eb0ba8
LatifovElbekjon <elbeklatifiy@gmail.com>
2025-10-03 16:59:24 +0500

    Add project files.

---
+++ FastFood.Models/SubCategory.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Models;
+
+public class SubCategory
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; } = string.Empty;
+
+    public int CategoryId  { get; set; }
+    public Category Category { get; set; }
+}
